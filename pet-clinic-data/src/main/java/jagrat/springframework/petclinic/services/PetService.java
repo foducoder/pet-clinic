caf78210c0329d630078d6c9bdff4b04432bc437
lolwa <lolwa@gmail.com>
2020-08-06 12:55:32 +0530

    closes #11.

--- pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/PetService.java
@@ -2,12 +2,6 @@ package jagrat.springframework.petclinic.services;
 
 import jagrat.springframework.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-     Pet findById(Long Id);
-
-     Pet save(Pet pet);
-
-     Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
--- pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/VetService.java
@@ -2,11 +2,6 @@ package jagrat.springframework.petclinic.services;
 
 import jagrat.springframework.petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-     Vet findById(Long Id);
-
-     Vet save(Vet vet);
-     Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
