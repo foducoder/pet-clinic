e8cba5c494002112bcdc9d573dda95441077610b
lolwa <lolwa@gmail.com>
2020-08-07 13:00:31 +0530

    closes #22.

--- pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package jagrat.springframework.petclinic.services.map;
 
 import jagrat.springframework.petclinic.model.Owner;
 import jagrat.springframework.petclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
--- pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package jagrat.springframework.petclinic.services.map;
 
 import jagrat.springframework.petclinic.model.Pet;
 import jagrat.springframework.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
