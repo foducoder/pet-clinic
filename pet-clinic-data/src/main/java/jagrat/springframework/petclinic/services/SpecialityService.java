74b04d5755583c505ddd61f0ad65e0f9420b2b22
lolwa <lolwa@gmail.com>
2020-08-11 16:08:40 +0530

    closes #31.

---
+++ pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package jagrat.springframework.petclinic.services;
+
+import jagrat.springframework.petclinic.model.Speciality;
+import jagrat.springframework.petclinic.services.CrudService;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
