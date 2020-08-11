43988e5bc7e3ee1ab2be84b3bf1eb27dd1aa22a4
lolwa <lolwa@gmail.com>
2020-08-11 10:55:38 +0530

    closes #29.

---
+++ pet-clinic-data/src/main/java/jagrat/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package jagrat.springframework.petclinic.services;
+
+import jagrat.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
